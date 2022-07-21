4c90da3d74ae6d104829dfdc8b745923097a304f
Jaqueline Fernandes <dhelly@gmail.com>
2022-07-21 11:38:17 -0300

    sentenças e comentários em Javascript

---
+++ 01_iniciando/comentario.js
@@ -0,0 +1,12 @@
+// comentário em javascript~
+console.log('Comentários não são exibidos no código');
+
+/**
+ * Comentários em JavaScript de múltiplas linhas
+ */
+ console.log('Comentários de múltiplas linhas');
+
+ /**
+  * Comentários devem ser relevantes. Principalmente em programas
+  * reais
+  */
---
+++ 01_iniciando/sentenca_1.js
@@ -0,0 +1,6 @@
+// Sentença de código
+//importante: A ordem importa
+console.log('Olá, mundo!');
+
+console
+    .log('Olá, mundo 2!');
